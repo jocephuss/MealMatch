804a4fcab5faf4cf99108b3d681212d66ec6de7e
Bryan Taboada <b.taboada123@gmail.com>
2024-10-03 19:20:22 -0600

    file structure, set up main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import DiscoverPage from "./pages/DiscoverPage";
+import ErrorPage from "./pages/ErrorPage";
+import FavoritePage from "./pages/FavoritePage"; 
+import LoginPage from "./pages/Login";
+import Signup from "./pages/Signup";
+
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <DiscoverPage />
+      }, {
+        path: '/favorite',
+        element: <FavoritePage />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/login',
+        element: <LoginPage />
+      }, 
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
