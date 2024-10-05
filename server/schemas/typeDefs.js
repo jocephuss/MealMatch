c3f2d76cb199f080901d5b60c1903a066d97388d
Bryan Taboada <b.taboada123@gmail.com>
2024-10-04 20:04:52 -0600

    boilerplate serverside schemas and models

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,33 @@
+// profile will be user's info
+// 
+const typeDefs = `
+type Profile {
+    _id: ID
+    name: String
+    email: String
+    password: String
+  }
+
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
+
+  type Query {
+    profiles: [Profile]!
+    profile(profileId: ID!): Profile
+  }
+
+  type Mutation {
+    addProfile(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    removeProfile(profileId: ID!): Profile
+  }
+
+`
+
+// might need to have a way to add favorites here, to be continued
+
+
+module.exports = typeDefs;
+
