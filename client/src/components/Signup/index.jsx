1bce47b425933d609581d4bf3fe08745c9ae9e9f
Jocephuss <rowlandjosiah14@gmail.com>
2024-10-04 18:45:46 -0600

    Messed with CSS for signup

--- client/src/components/Signup/index.jsx
+++ client/src/components/Signup/index.jsx
@@ -3,10 +3,11 @@ import React, { useState } from "react";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Signing up with:", { email, password });
+    console.log("Signing up with:", { email, password, username });
   };
 
   return (
@@ -16,13 +17,23 @@ const Signup = () => {
         <input
           type="email"
           placeholder="Email"
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
         <input
+          type="username"
+          placeholder="Username"
+          pattern="^[a-zA-Z0-9]{6,}$"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
           type="password"
           placeholder="Password"
+          pattern="[a-zA-Z0-9]{8,}$"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
